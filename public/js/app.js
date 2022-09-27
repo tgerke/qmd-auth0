0a9823c012b50e189c07275d849b08dab7092a6c
Travis Gerke <tgerke@mail.harvard.edu>
2022-09-27 14:06:56 -0700

    set up app

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
+let auth0 = null;
 
+// function to download values from auth_config.json
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+// function to download configuration file and initialize auth0
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+
+// handler to make the configuration call
+window.onload = async () => {
+  await configureClient();
+}
